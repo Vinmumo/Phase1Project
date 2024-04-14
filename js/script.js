b49dc66cbf04aabb6477605cebc62d4329ba7982
vincent <vinnymummo@gmail.com>
2024-04-14 19:23:27 +0300

    commits

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',async(event)=>{
     fetchDataAndSetupButton('kil', 'https://phase1project-qtim.onrender.com/Kilimani');
     fetchDataAndSetupButton('park', 'https://phase1project-qtim.onrender.com/Parklands');
     fetchDataAndSetupButton('Lang', 'https://phase1project-qtim.onrender.com/Langata');
-    fetchDataAndSetupButton('upper', 'https://phase1project-qtim.onrender.com/pperhill');
+    fetchDataAndSetupButton('upper', 'https://phase1project-qtim.onrender.com/Upperhill');
     fetchDataAndSetupButton('river', 'https://phase1project-qtim.onrender.com/RiverRoad');
       })
       // Function to clear previous contents and add new contents
